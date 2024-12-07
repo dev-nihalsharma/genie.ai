d957a105388441795e9f3e06413714fe760689c5
Nihal Sharma <nihalsharma@Trinity.local>
2024-12-07 17:03:48 +0530

    basic layout ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,134 @@
-import Image from "next/image";
+'use client';
+import { Spinner } from '@/components/ui/spinner';
+import { Logout02Icon } from 'hugeicons-react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [prompt, setPrompt] = useState('');
+  // const [promptHistory, setPromptHistory] = useState([]);
+  const [isPromptSubmiting, setIsPromptSubmiting] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const submitPrompt = async () => {
+    try {
+      setIsPromptSubmiting(true);
+      console.log(isPromptSubmiting);
+      setTimeout(function () {
+        console.log('hlo');
+        //your code to be executed after 1 second
+      }, 1000);
+
+      // save prompt message to local storage and also save the result to local storage {prompt: 'message', result: 'response'}
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsPromptSubmiting(false);
+    }
+  };
+
+  useEffect(() => {
+    const getPromptHistory = async () => {
+      try {
+        // setPromptHistory([]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getPromptHistory();
+  });
+
+  return (
+    <div className='h-screen bg-black text-white flex'>
+      {/* Sidebar */}
+      <div className='w-1/4 bg-gray-900 flex flex-col justify-between p-6 max-sm:hidden'>
+        <div>
+          {/* Logo */}
+          <div className='flex items-center space-x-2 mb-10'>
+            <div className='bg-white text-black rounded-full h-8 w-8 flex items-center justify-center font-bold'>
+              ⚡
+            </div>
+            <h1 className='text-xl font-semibold'>Larc AI</h1>
+          </div>
+
+          {/* Navigation */}
+          <nav className='space-y-4'>
+            <button className='bg-white text-black w-full text-left px-6 py-3 rounded-[50px] hover:bg-gray-300'>
+              New Chat
+            </button>
+            <button className=' w-full text-left px-6 py-3 rounded-[50px] hover:bg-gray-300 hover:text-black'>
+              Main Dashboard
+            </button>
+            <button className=' w-full text-left px-6 py-3 rounded-[50px] hover:bg-gray-300 hover:text-black'>
+              Profile Settings
+            </button>
+          </nav>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Chat History */}
+
+        {/* Profile */}
+        <div className='flex items-center space-x-3 justify-between'>
+          <div className='flex  space-x-3 items-center'>
+            <Image
+              src={'/tony-start.jpeg'}
+              width={50}
+              height={50}
+              alt='Profile Pic'
+              className='rounded-full'
+            />
+
+            <div className='text-sm'>
+              <h2 className='font-semibold'>Nihal Sharma</h2>
+              <h3>Basic User</h3>
+            </div>
+          </div>
+
+          <Logout02Icon className='cursor-pointer' />
+        </div>
+      </div>
+
+      {/* Main Content */}
+      <div className='flex-1 flex flex-col'>
+        {/* Header */}
+        <div className='flex justify-end p-6'>
+          <div className='bg-gray-800 text-sm flex items-center space-x-4 px-4 py-2 rounded-[50px]'>
+            <button className='bg-gray-600 rounded-full items-center  p-3'>
+              <Logout02Icon />
+            </button>
+
+            <Image
+              src={'/tony-start.jpeg'}
+              width={50}
+              height={50}
+              alt='Profile Pic'
+              className='rounded-full'
+            />
+          </div>
+        </div>
+
+        {/* Content Area */}
+        <div className='relative flex-1 flex items-center justify-center'>
+          <div className=' flex flex-col items-center w-full '>
+            <Image src={'/bg-image.png'} width={300} height={200} alt='bg image' />
+            <div className='absolute  bottom-10 flex flex-row  justify-between space-x-2 w-5/6'>
+              <input
+                type='text'
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                placeholder='Type your message here...'
+                className=' w-full bg-gray-800 px-4 py-3 rounded-lg outline-none focus:ring-2 focus:ring-gray-600'
+              />
+              <button
+                onClick={() => submitPrompt()}
+                className=' bg-gray-700 px-6 py-2 rounded-lg hover:bg-gray-600'
+              >
+                {isPromptSubmiting ? <Spinner /> : 'Submit'}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
